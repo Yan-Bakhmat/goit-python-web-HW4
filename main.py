46bd4eeeab9aea40043fb2069c6960c62d56b5cf
Yan-Bakhmat <yan.bakhmat@gmail.com>
2023-12-30 17:15:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from time import sleep
 import pathlib
 import urllib.parse
 import mimetypes
 import socket
+
+
+def echo_server(host, port):
+    with socket.socket() as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((host, port))
+        s.listen(1)
+        conn, addr = s.accept()
+        print(f"Connected by {addr}")
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                print(f'From client: {data}')
+                if not data:
+                    break
+                conn.send(data.upper())
+
+
+def simple_client(host, port):
+    with socket.socket() as s:
+        while True:
+            try:
+                s.connect((host, port))
+                s.sendall(b'Hello, world')
+                data = s.recv(1024)
+                print(f'From server: {data}')
+                break
+            except ConnectionRefusedError:
+                sleep(0.5)
 
 
 class HttpHandler(BaseHTTPRequestHandler):
