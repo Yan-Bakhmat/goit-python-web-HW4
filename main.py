4b6e5efc7f2c8b278c0cf44b86893ee0d78d136f
Yan-Bakhmat <yan.bakhmat@gmail.com>
2024-01-02 16:37:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,39 @@ import datetime
 
 UDP_IP = '127.0.0.1'
 UDP_PORT = 5000
-MESSAGE = None
+
+# secket server
+
+
+def run_server(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server = ip, port
+    sock.bind(server)
+    try:
+        while True:
+            data, address = sock.recvfrom(1024)
+            print(f'Received data: {data.decode()} from: {address}')
+            sock.sendto(data, address)
+            print(f'Send data: {data.decode()} to: {address}')
+
+    except KeyboardInterrupt:
+        print(f'Destroy server')
+    finally:
+        sock.close()
+
+# socket client
+
+
+def run_client(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server = ip, port
+    for line in MESSAGE.split(' '):
+        data = line.encode()
+        sock.sendto(data, server)
+        print(f'Send data: {data.decode()} to server: {server}')
+        response, address = sock.recvfrom(1024)
+        print(f'Response data: {response.decode()} from address: {address}')
+    sock.close()
 
 
 class HttpHandler(BaseHTTPRequestHandler):
@@ -64,39 +96,6 @@ def run(server_class=HTTPServer, handler_class=HttpHandler):
         http.serve_forever()
     except KeyboardInterrupt:
         http.server_close()
-
-# secket server
-
-
-def run_server(ip, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server = ip, port
-    sock.bind(server)
-    try:
-        while True:
-            data, address = sock.recvfrom(1024)
-            print(f'Received data: {data.decode()} from: {address}')
-            sock.sendto(data, address)
-            print(f'Send data: {data.decode()} to: {address}')
-
-    except KeyboardInterrupt:
-        print(f'Destroy server')
-    finally:
-        sock.close()
-
-# socket client
-
-
-def run_client(ip, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server = ip, port
-    for line in MESSAGE.split(' '):
-        data = line.encode()
-        sock.sendto(data, server)
-        print(f'Send data: {data.decode()} to server: {server}')
-        response, address = sock.recvfrom(1024)
-        print(f'Response data: {response.decode()} from address: {address}')
-    sock.close()
 
 
 if __name__ == '__main__':
